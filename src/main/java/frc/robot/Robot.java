1d340960fdbbbb9230caa6a963aa7b23dda87061
frc-617 <frc617@gmail.com>
2020-02-12 13:51:27 -0500

    Changed Code to use VictorSPX's
    Probably doesn't work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,11 +8,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.Vector2d;
+import edu.wpi.first.wpiutil.math.MathUtil;
 
-import javax.lang.model.util.ElementScanner6;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 /**
@@ -28,37 +29,49 @@ public class Robot extends TimedRobot {
 
   private static final int kJoystickChannel = 0;
 
-  private MecanumDrive m_robotDrive;
   private XboxController m_stick;
 
   private static final double m_maxSpeed = 0.4;
 
+  VictorSPX frontLeft = new VictorSPX(kFrontLeftChannel);
+  VictorSPX rearLeft = new VictorSPX(kRearLeftChannel);
+  VictorSPX frontRight = new VictorSPX(kFrontRightChannel);
+  VictorSPX rearRight = new VictorSPX(kRearRightChannel); 
+
   @Override
   public void robotInit() {
-    Spark frontLeft = new Spark(kFrontLeftChannel);
-    Spark rearLeft = new Spark(kRearLeftChannel);
-    Spark frontRight = new Spark(kFrontRightChannel);
-    Spark rearRight = new Spark(kRearRightChannel);
-
-    // Invert the left side motors.
-    // You may need to change or remove this to match your robot.
-    //frontLeft.setInverted(true);
-    //rearRight.setInverted(true);
-
-    m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
-
     m_stick = new XboxController(kJoystickChannel);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.setSafetyEnabled(true);
-    
 
     // Use the joystick X axis for lateral movement, Y axis for forward
     // movement, and Z axis for rotation.
-    m_robotDrive.driveCartesian(m_maxSpeed*m_stick.getRawAxis(4), -m_maxSpeed*m_stick.getRawAxis(1), m_maxSpeed*getZRotation());
-        m_robotDrive.feed();
+    driveCartesian(m_maxSpeed, m_stick.getRawAxis(4), -m_stick.getRawAxis(1), getZRotation());
+  }
+
+  private void driveCartesian(double m_maxSpeed, double ySpeed, double xSpeed, double zRotation)
+  {
+    ySpeed = MathUtil.clamp(ySpeed, -1.0, 1.0);
+    ySpeed = applyDeadband(ySpeed, 0.02);
+
+    xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
+    xSpeed = applyDeadband(xSpeed, 0.02);
+
+    Vector2d input = new Vector2d(ySpeed, xSpeed);
+    double[] wheelSpeeds = new double[4];
+    wheelSpeeds[0] = input.x + input.y + zRotation;
+    wheelSpeeds[1] = -input.x + input.y - zRotation;
+    wheelSpeeds[2] = -input.x + input.y + zRotation;
+    wheelSpeeds[3] = input.x + input.y - zRotation;
+
+    normalize(wheelSpeeds);
+
+    frontLeft.set(ControlMode.PercentOutput, wheelSpeeds[0]*m_maxSpeed);
+    rearLeft.set(ControlMode.PercentOutput, wheelSpeeds[1]*m_maxSpeed);
+    frontRight.set(ControlMode.PercentOutput, wheelSpeeds[2]*m_maxSpeed);
+    frontRight.set(ControlMode.PercentOutput, wheelSpeeds[3]*m_maxSpeed);
   }
 
   private double getZRotation() 
@@ -75,5 +88,33 @@ public class Robot extends TimedRobot {
         return 1.0;
       else
         return 0.0;
+  }
+
+  
+  private double applyDeadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private void normalize(double[] wheelSpeeds) {
+    double maxMagnitude = Math.abs(wheelSpeeds[0]);
+    for (int i = 1; i < wheelSpeeds.length; i++) {
+      double temp = Math.abs(wheelSpeeds[i]);
+      if (maxMagnitude < temp) {
+        maxMagnitude = temp;
+      }
+    }
+    if (maxMagnitude > 1.0) {
+      for (int i = 0; i < wheelSpeeds.length; i++) {
+        wheelSpeeds[i] = wheelSpeeds[i] / maxMagnitude;
+      }
+    }
   }
 }
